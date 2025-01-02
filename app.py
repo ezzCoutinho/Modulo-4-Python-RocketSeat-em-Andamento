31ac2af533f04e8461388a15d66f84d470381dc2
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-02 20:23:58 -0300

    Updating in app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User # Temos que trazer a modelagem dos dados, para o app
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your-secret-key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+# view login
+
 # Session <- conexão ativa
+
+@app.route("/login",  methods = ["POST"])
+def login():
+  data = request.json()
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    pass
+
+  return jsonify({"message": "Credenciais inválidas."})
 
 @app.route("/hello-world", methods = ["GET"])
 def hello_world():
